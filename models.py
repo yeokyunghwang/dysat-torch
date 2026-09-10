b38c1c43677073cc9286f3760b48455555d515fc
yeokyunghwang <yeokyungh@gmail.com>
2026-09-10 13:16:24 +0900

    Update models.py

--- models.py
+++ models.py
@@ -4,7 +4,8 @@ from layers import StructuralAttentionLayer, TemporalAttentionLayer
 
 class DySAT(nn.Module):
     """build_net port. One-hot input == a row lookup, so nn.Embedding."""
-    def __init__(self, n_nodes, T, num_features=128,
+    def __init__(self, n_nodes, T, 
+                 num_features=128, 
                  structural_layer_config=(128,), structural_head_config=(16,),
                  temporal_layer_config=128, temporal_head_config=16,
                  spatial_drop=0.1, temporal_drop=0.5):
@@ -18,10 +19,10 @@ class DySAT(nn.Module):
                                                    spatial_drop, spatial_drop))
             ind = out_dim
         self.struct = nn.ModuleList(layers)
-        self.temporal = TemporalAttentionLayer(temporal_layer_config,
-                                               temporal_head_config, T, temporal_drop)
+        self.temporal = TemporalAttentionLayer(ind, temporal_head_config, T, temporal_drop)
 
     def structural_one(self, src, dst, n):
+        """ src, dst : edge list (source & destination) | n = number of nodes """
         h = self.node_table.weight
         for layer in self.struct:
             h = layer(h, src, dst, n)
